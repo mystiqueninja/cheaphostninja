5ec564af21a00b7f1bf43490051a87893774da9c
Tyrell Daniel Nelson <cheaphostninja@gmail.com>
2015-04-05 23:44:58 +0200

    Modified menu

---
+++ public/javascripts/main.js
@@ -0,0 +1,41 @@
+jQuery(function ($) {
+  var header = $('header.max-size');
+  var nav = $('.navbar-default');
+  var bod = $('body');
+  var win = $(window);
+  var doc = $(document);
+  var isFixed = false;
+  //$('#scene').parallax("100%", 0.1);
+  header.css('height', win.height() - nav.height());
+  win.on('resize', function () {
+    header.css('height', win.height() - nav.height());
+  });
+  win.on('scroll', function () {
+    var scrollTop = doc.scrollTop();
+    if (scrollTop >= 200) {
+      nav.addClass('navbar-fixed-top');
+      if (isFixed === false) {
+        nav.hide();
+        nav.fadeIn(50, function () {
+          isFixed = true;
+        });
+      }
+      bod.css('paddingTop', nav.height());
+    } else {
+      nav.removeClass('navbar-fixed-top');
+      bod.css('paddingTop', 0);
+      isFixed = false;
+    }
+    $('.parallax').css('transform', 'translate(0px,' + scrollTop / 1.9 + 'px)');
+  });
+  $(".dropdown").hover(
+    function () {
+      $('.dropdown-menu', this).stop(true, true).slideDown(10);
+      $(this).toggleClass('open');
+    },
+    function () {
+      $('.dropdown-menu', this).stop(true, true).slideUp("fast");
+      $(this).toggleClass('open');
+    }
+  );
+});
